fed891f3fc76506c4ebdfcdd569c6cc01f2264cb
Edgar Marukyan <emarukyan@gmail.com>
2022-02-07 18:03:47 +0400

    fix(src): code review.

--- src/components/functions/getCharacterCoords.js
+++ src/components/functions/getCharacterCoords.js
@@ -1,11 +1,21 @@
 export const getCharacterCoords = (board, boardSize, character) => {
-    const pos = [];
-    for (let i = 0; i < boardSize; i++) {
-        for (let j = 0; j < boardSize; j++) {
-            if (board[i][j] === character.id) {
-                pos.push([i, j]);
-            }
-        }
+  const pos = []
+  for (let i = 0; i < boardSize; i++) {
+    for (let j = 0; j < boardSize; j++) {
+      if (board[i][j] === character.id) {
+        pos.push([i, j])
+      }
     }
-    return pos;
+  }
+  return pos
+
+  //   return boardSize.reduce((acc, row, i) => {
+  //     row.reduce((_, cell, j) => {
+  //       if (cell === character.id) {
+  //         acc.push(i, j)
+  //       }
+  //     })
+
+  //     return acc
+  //   }, [])
 }
--- src/components/functions/moveRabbit.js
+++ src/components/functions/moveRabbit.js
@@ -1,48 +1,41 @@
-import { setBoard } from "./setBoard";
+import { setBoard } from "./setBoard"
 import { FREE_CELL, HOUSE_CELL, WOLF_CELL } from "../Constants"
-import { gameover } from "./gameOver";
-import { setCharacterCoords } from "./setCharacterCoords";
+import { gameover } from "./gameOver"
+import { setCharacterCoords } from "./setCharacterCoords"
 
 export const clearCoord = ([x, y], board) => {
-    setBoard([x, y], FREE_CELL, board)
+  setBoard([x, y], FREE_CELL, board)
 }
 export const changePosition = (direction, rabbitPos, boardSize) => {
-    const [x, y] = rabbitPos;
-    let newX = x, newY = y;
-    if (direction === 'ArrowUp') {
-        newX = (x - 1) < 0 ? (boardSize - 1) : x - 1;
-    }
-    else if (direction === 'ArrowDown') {
-        newX = (x + 1) === boardSize ? 0 : x + 1;
-    }
-    else if (direction === 'ArrowLeft') {
-        newY = (y - 1) < 0 ? (boardSize - 1) : y - 1;
-    }
-    else if (direction === 'ArrowRight') {
-        newY = (y + 1) === boardSize ? 0 : y + 1;
-    }
-    return [newX, newY];
+  const [x, y] = rabbitPos
+  let newX = x,
+    newY = y
+  if (direction === "ArrowUp") {
+    newX = x - 1 < 0 ? boardSize - 1 : x - 1
+  } else if (direction === "ArrowDown") {
+    newX = x + 1 === boardSize ? 0 : x + 1
+  } else if (direction === "ArrowLeft") {
+    newY = y - 1 < 0 ? boardSize - 1 : y - 1
+  } else if (direction === "ArrowRight") {
+    newY = y + 1 === boardSize ? 0 : y + 1
+  }
+  return [newX, newY]
 }
 
-export const isCellFree = ([newX, newY], board) => {
-    return board[newX][newY] === FREE_CELL;
-}
+export const isCellFree = ([x, y], board) => board[x][y] === FREE_CELL
 
 export const checkForGameOver = ([x, y], board, character, gameOverData) => {
-    if (board[x][y] === HOUSE_CELL) {
-        gameOverData.wolvesWon = false;
-        gameover(gameOverData);
-    }
-    else if (board[x][y] === WOLF_CELL) {
-        gameOverData.wolvesWon = true;
-        gameover(gameOverData);
-    }
-    else {
-        setCharacterCoords(board, [x, y], character)
-    }
+  if (board[x][y] === HOUSE_CELL) {
+    gameOverData.wolvesWon = false
+    gameover(gameOverData)
+  } else if (board[x][y] === WOLF_CELL) {
+    gameOverData.wolvesWon = true
+    gameover(gameOverData)
+  } else {
+    setCharacterCoords(board, [x, y], character)
+  }
 }
+
 export const ifPositionIsLegal = ([x, y], character, board) => {
-    return character.allowedMoves.includes(board[x][y]);
+  return character.allowedMoves.includes(board[x][y])
 }
-
-
